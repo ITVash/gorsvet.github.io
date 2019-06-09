6e1221c902191123c1fbc67f8171897ba3c1917d
ITVash <vash_dn@mail.ru>
2019-06-10 02:17:59 +0300

    Upp Fix

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,25 @@
 'use strict';
 const navz = document.querySelector('.nav');
-const head = document.querySelector('header');
-const hTop = head.getBoundingClientRect().top;
-//console.log(hTop);
+const nav_brand = document.querySelector('.nav__brand .container .row');
+const logo = document.querySelector('.nav__brand_logo');
+const phone = document.querySelector('.nav__brand_telephon');
+const menu = document.querySelector('.nav__menu .container .row');
+const btn = document.querySelector('.nav__menu_btn');
+btn.addEventListener('click', e => {
+  e.preventDefault();
+  navz.classList.toggle('activ');
+});
 window.addEventListener('scroll', () => {
   if (this.scrollY >= 140) {
     navz.classList.add('fixed');
+    //menu.appendChild(logo);
+    menu.prepend(logo);
+    menu.append(phone);
+    //menu.appendChild(phone);
   } else if (this.scrollY <= 139) {
     navz.classList.remove('fixed');
+    nav_brand.prepend(logo);
+    menu.append(phone);
+    nav_brand.appendChild(phone);
   }
 });
