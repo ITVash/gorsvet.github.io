ce789afeb74e6881b836d54aeaf258ed93e3e82d
ITVash <vash_dn@mail.ru>
2019-06-15 09:27:43 +0300

    Fix

--- app/js/index.js
+++ app/js/index.js
@@ -4,12 +4,26 @@ const nav_brand = document.querySelector('.nav__brand .container .row');
 const logo = document.querySelector('.nav__brand_logo');
 const phone = document.querySelector('.nav__brand_telephon');
 const menu = document.querySelector('.nav__menu .container .row');
+const nav__menu = document.querySelector('.nav__menu');
+const nav__menu_mobile = document.querySelector('.nav__menu .mobile');
 const btn = document.querySelector('.nav__menu_btn');
 const menuCol = document.querySelector('.nav__menu .container .row .mobile');
 btn.addEventListener('click', e => {
   e.preventDefault();
   navz.classList.toggle('activ');
-  menuCol.classList.toggle('show');
+  //menuCol.classList.toggle('show');
+  if (menuCol.classList.contains('show')) {
+    nav__menu_mobile.classList.remove('show');
+    setTimeout( () => {
+      menu.append(nav__menu_mobile);
+    }, 1010);
+  } else {
+    nav__menu.append(menuCol);
+    setTimeout( () => {
+      nav__menu_mobile.classList.add('show');
+    }, 10);
+    
+  }
 });
 window.addEventListener('scroll', () => {
   if (this.scrollY >= 140) {
